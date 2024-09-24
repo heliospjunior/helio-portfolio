4ff68a86273b3dde1e0043cba880f829a463da48
Hélio S P Júnior <heliospjunior@gmail.com>
2024-09-24 10:58:34 -0300

    Update portfolio

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,29 +22,42 @@ function updateProfileInfo(profileData) {
 
 }
 
-function updateSoftSkills(profileData){
+function updateSoftSkills(profileData) {
     const softSkills = document.getElementById('profile.skills.softSkills')
 
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
 }
 
-function updateHardSkills(profileData){
+function updateHardSkills(profileData) {
     const hardSkills = document.getElementById('profile.skills.hardSkills')
 
     hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.title}"></li>`).join('');
 }
 
-function updateLanguages(profileData){
+function updateLanguages(profileData) {
     const languages = document.getElementById('profile.languages')
-    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`)
+    languages.innerHTML = profileData.languages.map(language => `<li>${language}</li>`).join('');
 
 }
 
+function updatePortfolio(profileData) {
+    const portfolio = document.getElementById('profile.porrtfolio')
+    portfolio.innerHTML = profileData.portfolio.map(project => {
+        return `
+        <li>
+            <h3 ${project.github ? 'class="github"' : ''}>${project.name}</h3>
+            <a href="${project.url}" target="_blank">${project.url}</a>
+        </li>
+        `
+    }).join('');
+}
 
-( async () => {
+
+(async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
     updateHardSkills(profileData)
     updateLanguages(profileData)
+    updatePortfolio(profileData)
 })()
