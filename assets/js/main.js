e15afbe1d63d6f6843cf3d984bc249096a517c89
Hélio S P Júnior <heliospjunior@gmail.com>
2024-09-24 09:12:04 -0300

    Update languages

--- assets/js/main.js
+++ assets/js/main.js
@@ -28,9 +28,22 @@ function updateSoftSkills(profileData){
     softSkills.innerHTML = profileData.skills.softSkills.map(skill => `<li>${skill}</li>`).join('');
 }
 
+function updateHardSkills(profileData){
+    const hardSkills = document.getElementById('profile.skills.hardSkills')
+
+    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.title}"></li>`).join('');
+}
+
+function updateLanguages(profileData){
+    const languages = document.getElementById('profile.languages')
+    languages.innerHTML = profileData.languages.map(language => `<li>${languages}</li>`)
+
+}
+
 
 ( async () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData)
+    updateHardSkills(profileData)
 })()
